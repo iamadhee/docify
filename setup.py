761d12d80e9afb5251a48d024a9d5569e836cf05
Adheeban Manoharan <69910091+iamadhee@users.noreply.github.com>
2023-04-14 22:00:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["requests","openai","tqdm","pathlib"],
+    install_requires=["requests","openai","tqdm","pathlib","pyyaml"],
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
     entry_points={
